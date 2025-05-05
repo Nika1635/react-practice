34b207b39ff0e0328f1bb3b6ebea6da4cb05e183
nika robakidze <nika70252@gmail.com>
2025-05-05 22:03:05 +0400

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
@@ -6,5 +5,13 @@ const root = createRoot(document.getElementById("root"))
 root.render(
   <section>
     <img src="react-logo.png" width = "40px"></img>
+    <h1>Fun facts about react</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </section>
 )
