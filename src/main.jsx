d523f945a76e8bc465261a6066f0553382b2a2e5
nika robakidze <nika70252@gmail.com>
2025-05-05 21:56:31 +0400

    first

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <section>
+    <img src="react-logo.png" width = "40px"></img>
+  </section>
+)
