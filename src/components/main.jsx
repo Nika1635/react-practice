e016ffa963c079afd100474aaa5be2e740ed1771
nika robakidze <nika70252@gmail.com>
2025-05-07 14:51:46 +0400

    main-component

---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react"
+
+export default function Main(){
+    return (
+        <Fragment>
+            <h1>Fun facts about react</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </Fragment>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import { createRoot } from "react-dom/client"
+import { Fragment } from "react"
 import  Header  from "./components/header.jsx"
+import Main from "./components/main.jsx"
 
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-  <section>
-    <Header/>
-    <h1>Fun facts about react</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </section>
+  <Fragment>
+    <Header />
+    <Main />
+  </Fragment>
 )
